b1df15a5bde8b28fd52f251bbb1293284854a30b
Geovana <geovanacarvalhodossantos16@gmail.com>
2025-11-05 17:59:48 -0300

    Adição: Função login para adicionar notas e atividades

--- back-end/code.c
+++ back-end/code.c
@@ -107,23 +107,119 @@ void realizarCadastro(){
 
     FILE *arquivo_usuario = fopen("dados_usuarios.txt", "a");
     if (arquivo_usuario != NULL) {
-        fprintf(arquivo_usuario, "Cargo: %s\n", cargo_texto);
-        fprintf(arquivo_usuario, "Nome: %s\n", nome);
-        fprintf(arquivo_usuario, "Email: %s\n", email);
+        fprintf(arquivo_usuario, "%s,%s,%s,%s",
+                email,
+                senha_digitada,
+                cargo_texto,
+                nome);
 
         if (tipo_usuario == 1 || tipo_usuario == 2) {
-            fprintf(arquivo_usuario, "Turma: %s\n", turma);
+            fprintf(arquivo_usuario, ",%s", turma);
         }
 
-        fprintf(arquivo_usuario, "Senha: %s\n", senha_digitada);
         fprintf(arquivo_usuario, "\n");
 
         fclose(arquivo_usuario);
-        printf("\nUsuário cadastrado com sucesso!\n", nome);
+        printf("\nUsuário cadastrado com sucesso!\n");
 
     } else {
         perror("Erro ao abrir arquivo dados_usuarios.txt");
     }
+}
+
+void fazer_login(void){
+    char email_digitado[100];
+    char senha_digitada[32];
+
+    puts("=======================================");
+    puts("REALIZAR LOGIN ANTES DE INSERIR A NOTA");
+    puts("=======================================");
+
+    printf("Digeite seu email: %s", email_digitado);
+    scanf("%99s", &email_digitado);
+
+    while (getchar() != '\n');
+        printf("Digeite seu email: %s", senha_digitada);
+        obterSenhaEscondida(senha_digitada, 32);
+
+    FILE *arquivo_usuario = fopen("dados_usuarios.txt", "r");
+    if (arquivo_usuario == NULL){
+        perror("Erro ao abrir dados_usuarios.txt");
+        return;
+    }
+
+    int login_sucesso = 0;
+    char linha_do_arquivo[256];
+
+    char email_bloco_atual[100] = "";
+    char senha_bloco_atual[32] = "";
+    char cargo_bloco_atual[20] = "";
+    char cargo_LOGADO[20] = "";
+
+    while (fgets(linha_do_arquivo, sizeof(linha_do_arquivo), arquivo_usuario)){
+        if (linha_do_arquivo[0] == '\n'){
+            if (strcmp(email_digitado, email_bloco_atual) == 0 && strcmp(senha_digitada, senha_bloco_atual) == 0){
+                login_sucesso = 1;
+                strcpy(cargo_LOGADO, cargo_bloco_atual);
+                break;
+            }
+            strcpy(email_bloco_atual, "");
+            strcpy(senha_bloco_atual, "");
+            strcpy(cargo_bloco_atual, "");
+            continue;
+        }
+        char chave[100];
+        char valor[100];
+
+        if(sscanf(linha_do_arquivo, "%99[^:]: %[^\n]", chave, valor) == 2) {
+            if (strcmp(chave, "Email") == 0) {
+                strcpy(email_bloco_atual, valor);
+            } else if (strcmp(chave, "Senha") == 0) {
+                strcpy(senha_bloco_atual, valor);
+            } else if (strcpy(chave, "Cargo")== 0){
+                strcpy(cargo_bloco_atual, valor);
+            }
+        }
+    }
+
+    if (login_sucesso == 0) {
+        if (strcmp(email_digitado, email_bloco_atual) == 0 &&
+            strcmp(senha_digitada, senha_bloco_atual) == 0){
+            login_sucesso = 1;
+            strcpy(cargo_LOGADO, cargo_bloco_atual);
+        }
+
+    fclose(arquivo_usuario);
+
+    if (login_sucesso == 1){
+        printf("\nLogin realizado com sucesso! (Cargo: %s)\n", cargo_LOGADO);
+        if (strcmp(cargo_LOGADO, "Docente") == 0 ||
+            strcmp(cargo_LOGADO, "Administrador") == 0){
+                printf("\nLogin realizado com sucesso! Bem-vindo(a)!");
+                puts("==============================================");
+                puts("Selecione uma opção:\n");
+                int opcao_sub_menu;
+                printf("(1) Inserir notas\n(2) Inserir atividades\nOpção: %d", opcao_sub_menu);
+                if (scanf("%d", &opcao_sub_menu) != 1) {
+                    printf("Entrada inválida. Digite um número.\n");
+                    while (getchar() != '\n');
+                } else {
+                    while (getchar() != '\n');
+                        if (opcao_sub_menu == 1){
+                            registro_notas();
+                        } else if (opcao_sub_menu == 2){
+                            incluir_atividade();
+                        } else {
+                            printf("Opção inválida. Escolha 1 ou 2.\n");
+                        }
+                }
+            } else {
+                printf("\nAcesso negado. Esta função é reservada para Docentes e Admin.\n");
+            }
+
+        } else {
+            puts("\nEmail ou senha incorretos.");
+        }
 }
 
 int main() {
@@ -154,7 +250,7 @@ int main() {
                 realizarCadastro();
                 break;
             case 2:
-                printf("Função 'Inserir notas' ainda não implementada.\n");
+                fazer_login();
                 break;
             case 3:
                 printf("Saindo...\n");
